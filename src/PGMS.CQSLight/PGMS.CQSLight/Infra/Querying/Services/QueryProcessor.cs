19b869f03f8708d13b912554de8abaecd4d84115
Philippe Grandmont <philippe.grandmont@gmail.com>
2021-03-25 11:16:36 -0400

    Query Processor - Add Async Methods

--- src/PGMS.CQSLight/PGMS.CQSLight/Infra/Querying/Services/QueryProcessor.cs
+++ src/PGMS.CQSLight/PGMS.CQSLight/Infra/Querying/Services/QueryProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Autofac;
 using PGMS.Data.Services;
 
@@ -8,6 +9,9 @@ namespace PGMS.CQSLight.Infra.Querying.Services
     {
         TResult Process<TResult>(IQuery<TResult> query);
 		TResult ProcessWithCaching<TResult>(IQuery<TResult> query, int cacheDurationInSeconds);
+
+		Task<TResult> ProcessAsync<TResult>(IQuery<TResult> query);
+		Task<TResult> ProcessWithCachingAsync<TResult>(IQuery<TResult> query, int cacheDurationInSeconds);
 
 	}
 
@@ -56,6 +60,43 @@ namespace PGMS.CQSLight.Infra.Querying.Services
 			}
 
 			var result = Process(query);
+			cacheRepository.Set(query, result, DateTime.Now.AddSeconds(cacheDurationInSeconds));
+
+			return result;
+		}
+
+		public async Task<TResult> ProcessAsync<TResult>(IQuery<TResult> query)
+		{
+			var asyncHandlerType = typeof(IHandleQueryAsync<,>).MakeGenericType(query.GetType(), typeof(TResult));
+			dynamic asyncHandler;
+			if (context.TryResolve(asyncHandlerType, out asyncHandler))
+			{
+				return await asyncHandler.Handle((dynamic)query);
+			}
+
+			var asyncEnumeratorHandlerType = typeof(IHandleQueryAsyncEnumerable<,>).MakeGenericType(query.GetType(), typeof(TResult));
+			dynamic asyncEnumHandler;
+			if (context.TryResolve(asyncEnumeratorHandlerType, out asyncEnumHandler))
+			{
+				return asyncEnumHandler.Handle((dynamic)query);
+			}
+
+			var handlerType = typeof(IHandleQuery<,>).MakeGenericType(query.GetType(), typeof(TResult));
+			dynamic handler = context.Resolve(handlerType);
+
+			return handler.Handle((dynamic)query);
+		}
+
+
+		public async Task<TResult> ProcessWithCachingAsync<TResult>(IQuery<TResult> query, int cacheDurationInSeconds)
+		{
+			var cached = cacheRepository.Get(query);
+			if (cached != null)
+			{
+				return (TResult)cached;
+			}
+
+			var result = await ProcessAsync(query);
 			cacheRepository.Set(query, result, DateTime.Now.AddSeconds(cacheDurationInSeconds));
 
 			return result;
