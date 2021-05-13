42649b6958c2f7732be2f8a6fab9880bf3888608
Philiippe Grandmont <philippe.grandmnt@gmail.com>
2021-05-13 14:35:27 -0400

    ContextFactory.Create virtual

--- src/PGMS.CQSLight/PGMS.DataProvider.EFCore/Services/ContextFactory.cs
+++ src/PGMS.CQSLight/PGMS.DataProvider.EFCore/Services/ContextFactory.cs
@@ -4,22 +4,20 @@ namespace PGMS.DataProvider.EFCore.Services
 {
     public abstract class ContextFactory<T> where T : DbContext
     {
-        public T Create(string connectionString)
+	   
+        public virtual T Create(string connectionString)
         {
             var optionsBuilder = new DbContextOptionsBuilder<T>();
             optionsBuilder.UseSqlServer(connectionString);
 
             var context = CreateContext(optionsBuilder.Options);
-            //Ensure database creation
-            //context.Database.EnsureCreated();
-
+            
             return context;
         }
 
         public abstract T CreateContext(DbContextOptions<T> options);
     }
 
-    
 
     public interface IConnectionStringProvider
     {
