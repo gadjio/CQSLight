2d27d6ee4ba9ff6df6f227a4c7241a1de710fe2f
Philippe Grandmont <philippe.grandmont@gmail.com>
2021-05-28 17:11:08 -0400

    Config to disable autoFlush in DirectBus

--- src/PGMS.CQSLight/PGMS.CQSLight/Installers/CqsLightInfraInstaller.cs
+++ src/PGMS.CQSLight/PGMS.CQSLight/Installers/CqsLightInfraInstaller.cs
@@ -2,16 +2,37 @@
 using System.Collections.Generic;
 using System.Reflection;
 using Autofac;
+using PGMS.CQSLight.Infra.Commands.Services;
 
 namespace PGMS.CQSLight.Installers
 {
 	public class CqsLightInfraInstaller
 	{
-		public static void ConfigureServices(ContainerBuilder builder)
+		public static void ConfigureServices(ContainerBuilder builder, IDirectBusConfigurationProvider config)
 		{
+			builder.Register(c => config).As<IDirectBusConfigurationProvider>().SingleInstance();
 			builder.RegisterAssemblyTypes(typeof(CqsLightInfraInstaller).GetTypeInfo().Assembly)
 				.Where(t => t.Name.EndsWith("Service") || (t.Namespace != null && t.Namespace.Contains(".Services")))
 				.AsImplementedInterfaces();
 		}
+
+		public static void ConfigureServices(ContainerBuilder builder, bool useAutoFlush = false)
+		{
+			var config = new DirectBusConfigurationProvider(useAutoFlush);
+			builder.Register(c => config).As<IDirectBusConfigurationProvider>().SingleInstance();
+			builder.RegisterAssemblyTypes(typeof(CqsLightInfraInstaller).GetTypeInfo().Assembly)
+				.Where(t => t.Name.EndsWith("Service") || (t.Namespace != null && t.Namespace.Contains(".Services")))
+				.AsImplementedInterfaces();
+		}
+	}
+
+	public class DirectBusConfigurationProvider : IDirectBusConfigurationProvider
+	{
+		public DirectBusConfigurationProvider(bool useAutoFlush)
+		{
+			UseAutoFlush = useAutoFlush;
+		}
+
+		public bool UseAutoFlush { get; }
 	}
 }
